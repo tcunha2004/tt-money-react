54b330615a4743b6ef143050d4e03a02204dfc95
tcunha2004 <tcunha2004@gmail.com>
2025-08-08 10:58:41 -0300

    add: Income and Output buttons in Modal

--- src/components/NewTransactionalModal/index.tsx
+++ src/components/NewTransactionalModal/index.tsx
@@ -1,13 +1,20 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 import { MdClose } from "react-icons/md";
+import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
 
 function NewTransactionalModal() {
   return (
     <Dialog.Portal>
       <Overlay />
       <Content>
-        <Dialog.Title>AAAAAA</Dialog.Title>
+        <Dialog.Title>Nova Transação</Dialog.Title>
         <CloseButton>
           <MdClose size={24} />
         </CloseButton>
@@ -16,6 +23,18 @@ function NewTransactionalModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <FaRegArrowAltCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <FaRegArrowAltCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
