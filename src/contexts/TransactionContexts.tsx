c406e528a6e718c8d6d9b64d45d63b04eff75e88
tcunha2004 <tcunha2004@gmail.com>
2025-08-11 15:55:11 -0300

    add: Creating "transactions context" for all components

---
+++ src/contexts/TransactionContexts.tsx
@@ -0,0 +1,43 @@
+import { createContext, useEffect, useState, type ReactNode } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionContext = createContext({} as TransactionContextType);
+
+function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+}
+
+export default TransactionsProvider;
