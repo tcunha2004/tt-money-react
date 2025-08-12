74e23865471bdfb855ab60355c3cd8681f19250e
tcunha2004 <tcunha2004@gmail.com>
2025-08-12 10:34:18 -0300

    add: Calculating Summary Cards

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,30 @@
 import { MdCurrencyExchange } from "react-icons/md";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
+import { useContext } from "react";
+import { TransactionContext } from "../../contexts/TransactionContexts";
 
 function Summary() {
+  const { transactions } = useContext(TransactionContext);
+
+  const pricesSummary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type == "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,21 +32,21 @@ function Summary() {
           <span>Entradas</span>
           <FaRegArrowAltCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 18.007,00</strong>
+        <strong>{pricesSummary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <FaRegArrowAltCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 18.007,00</strong>
+        <strong>{pricesSummary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <MdCurrencyExchange size={32} color="#fff" />
         </header>
-        <strong>R$ 18.007,00</strong>
+        <strong>{pricesSummary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
