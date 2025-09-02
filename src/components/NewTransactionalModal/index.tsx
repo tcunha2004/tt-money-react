99cdaa249807ccf0a9f8580fad79c34755812e49
tcunha2004 <tcunha2004@gmail.com>
2025-09-02 15:37:29 -0300

    update: Adding Controlled Forms - #D094

--- src/components/NewTransactionalModal/index.tsx
+++ src/components/NewTransactionalModal/index.tsx
@@ -8,8 +8,27 @@ import {
 } from "./styles";
 import { MdClose } from "react-icons/md";
 import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  desc: string;
+  price: number;
+  category: string;
+}
 
 function NewTransactionalModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<FormData>();
+
+  async function handleNewTransaction(data: FormData) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(data.desc);
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -19,10 +38,25 @@ function NewTransactionalModal() {
           <MdClose size={24} />
         </CloseButton>
 
-        <form action="">
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register("desc")}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register("price", { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
 
           <TransactionType>
             <TransactionTypeButton variant="income" value="income">
@@ -36,7 +70,9 @@ function NewTransactionalModal() {
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,28 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  query: string;
+}
 
 function SearchForm() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<FormData>();
+
+  async function handleSearchTransactions(data: FormData) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data.query);
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
+      <button type="submit" disabled={isSubmitting}>
         <FaMagnifyingGlass size={20} /> Buscar
       </button>
     </SearchFormContainer>
